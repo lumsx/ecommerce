0c6a91948cbca41e9498a05ad1d216d3a10ff35a
Haseeb <muhammad.haseeb@edly.io>
2020-06-09 14:54:43 +0500

    Updated logout url check logic

--- ecommerce/extensions/edly_ecommerce_app/middleware.py
+++ ecommerce/extensions/edly_ecommerce_app/middleware.py
@@ -49,5 +49,5 @@ class EdlyOrganizationAccessMiddleware(object):
         user_is_superuser = request.user.is_superuser
         if user_is_authenticated and not user_is_superuser and not user_has_edly_organization_access(request):
             logger.exception('Edly user %s has no access for site %s.' % (request.user.email, request.site))
-            if request.path != '/logout':
+            if 'logout' not in request.path:
                 return HttpResponseRedirect(reverse('logout'))

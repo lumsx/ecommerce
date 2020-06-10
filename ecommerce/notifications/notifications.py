39d5e78e4c4d862702fa6d5c77a2fc01232e285f
Burhan Nasir <burhandodhy@gmail.com>
2020-06-10 14:46:14 +0500

    Apply branding and theming on Order confirmation Email(#26)

--- ecommerce/notifications/notifications.py
+++ ecommerce/notifications/notifications.py
@@ -4,6 +4,7 @@ from oscar.core.loading import get_class, get_model
 from premailer import transform
 
 from ecommerce.extensions.analytics.utils import parse_tracking_context
+from ecommerce.extensions.edly_ecommerce_app.context_processor import get_theme_colors
 
 log = logging.getLogger(__name__)
 CommunicationEventType = get_model('customer', 'CommunicationEventType')
@@ -25,11 +26,17 @@ def send_notification(user, commtype_code, context, site):
     tracking_pixel = 'https://www.google-analytics.com/collect?v=1&t=event&ec=email&ea=open&tid={tracking_id}' \
                      '&cid={client_id}&uip={ip}'.format(tracking_id=tracking_id, client_id=client_id, ip=ip)
     full_name = user.get_full_name()
+    configuration_helpers = site.siteconfiguration.edly_client_theme_branding_settings
+
     context.update({
         'full_name': full_name,
         'site_domain': site.domain,
         'platform_name': site.name,
         'tracking_pixel': tracking_pixel,
+        'edly_mailing_address': configuration_helpers.get('CONTACT_MAILING_ADDRESS'),
+        'edly_copyright_text': configuration_helpers.get('EDLY_COPYRIGHT_TEXT'),
+        'edly_branding_config': configuration_helpers.get('BRANDING'),
+        'edly_colors_config': get_theme_colors(configuration_helpers)
     })
 
     try:
